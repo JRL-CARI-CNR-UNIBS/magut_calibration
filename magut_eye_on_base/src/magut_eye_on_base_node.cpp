f96ec2f2e5ed4e68770fd31bd4fe40410e02c7e3
Manuel Beschi <manuel.beschi@unibs.it>
2021-09-14 22:24:05 +0200

    major bugfixes

--- magut_eye_on_base/src/magut_eye_on_base_node.cpp
+++ magut_eye_on_base/src/magut_eye_on_base_node.cpp
@@ -84,8 +84,10 @@ int main(int argc, char **argv)
     return 0;
   }
 
+  double amplitude=0.2;
+  double amplitude_rot=15.0/180*M_PI;
 
-  ROS_INFO("Move the robot to the initial position (the robot should be able to move around 200mm in each direction)");
+  ROS_INFO("Move the robot to the initial position (the robot should be able to move around 200mm  and 15 degree in each direction)");
   ROS_INFO("Afterthat, be sure the robot is stopped");
 
 
@@ -142,23 +144,27 @@ int main(int argc, char **argv)
   ROS_DEBUG_STREAM("Transform "<<camera_base_link<<" <= "<<camera_marker_link<<"\n"<<T_camera_marker_orig.matrix());
 
   // X DIRECTION
-  ROS_INFO("\n\nMove the robot in the X direction around +/- 200mm . Afterthat");
+  ROS_INFO("The robot will move in X direction around +/-%f mm",amplitude*1000);
 
   geometry_msgs::Pose actual_pose = move_group.getCurrentPose().pose;
   geometry_msgs::Pose target_pose=actual_pose;
+  geometry_msgs::Pose initial_pose=actual_pose;
   moveit::planning_interface::MoveGroupInterface::Plan plan;
   bool success;
+
+  moveit::core::RobotState initial_state(*move_group.getCurrentState());
+
   move_group.setStartState(*move_group.getCurrentState());
   const moveit::core::JointModelGroup* joint_model_group =move_group.getCurrentState()->getJointModelGroup(group_name);
   ROS_INFO("reference_frame %s",move_group.getPoseReferenceFrame().c_str());
 
+  move_group.setStartState(*move_group.getCurrentState());
+
   while(ros::ok())
   {
-    move_group.setStartState(*move_group.getCurrentState());
-    actual_pose = move_group.getCurrentPose(robot_end_link).pose;
     double delta_x=0;
     while (std::abs(delta_x)<0.05)
-      delta_x=-0.200+0.4*(1.0*(double)std::rand()/((double)RAND_MAX));
+      delta_x=(-1.0+2.0*(1.0*(double)std::rand()/((double)RAND_MAX)))*amplitude;
     ROS_INFO("delta_x=%f",delta_x);
 
     target_pose.position.x=actual_pose.position.x+delta_x;
@@ -186,30 +192,8 @@ int main(int argc, char **argv)
       break;
     }
   }
+  ros::Duration(2).sleep();
 
-
-  while (ros::ok())
-  {
-    ROS_INFO("Press");
-    ROS_INFO("- '1' and ENTER to continue");
-    ROS_INFO("- '-10' and ENTER to stop");
-
-    std::string input;
-    std::cin >> input;
-    try
-    {
-      y = boost::lexical_cast<int> (input);
-    }
-    catch (boost::bad_lexical_cast const &)
-    {
-      ROS_ERROR("Error: input string was not valid.");
-      continue;
-    }
-    if (y==-10)
-      return 0;
-    if (y==1)
-      break;
-  }
 
   while (ros:: ok())
   {
@@ -239,11 +223,12 @@ int main(int argc, char **argv)
   ROS_INFO_STREAM("delta = " << (T_base_tool_x.translation()-T_base_tool_orig.translation()).transpose());
   double distance_x_camera=(T_camera_marker_x.translation()-T_camera_marker_orig.translation()).norm();
   ROS_INFO("The amplitude of the movement recorded by the camera is %f mm",distance_x_camera*1e3);
-
+  Eigen::Vector3d x_robot_in_camera=(T_camera_marker_x.translation()-T_camera_marker_orig.translation())/distance_x;
 
 
   // Y DIRECTION
-  ROS_INFO("\n\nMove the robot in the Y direction around +/- 200mm . Afterthat");
+  ROS_INFO("The robot will move in Y direction around +/-%f mm",amplitude*1000);
+  target_pose=initial_pose;
   while(ros::ok())
   {
     move_group.setStartState(*move_group.getCurrentState());
@@ -251,7 +236,7 @@ int main(int argc, char **argv)
     while (std::abs(delta_y)<0.05)
       delta_y=-0.200+0.4*(1.0*(double)std::rand()/((double)RAND_MAX));
     ROS_INFO("delta_y=%f",delta_y);
-    target_pose.position.y=actual_pose.position.y+delta_y;
+    target_pose.position.y=initial_pose.position.y+delta_y;
     ROS_DEBUG_STREAM("Actual\n"<<actual_pose<<"\nTarget=\n"<<target_pose);
     robot_state::RobotState target_state(*move_group.getCurrentState());
     if (!target_state.setFromIK(joint_model_group,target_pose)) continue;
@@ -276,30 +261,8 @@ int main(int argc, char **argv)
       break;
     }
   }
+  ros::Duration(2).sleep();
 
-
-  while (ros::ok())
-  {
-    ROS_INFO("Press");
-    ROS_INFO("- '1' and ENTER to continue");
-    ROS_INFO("- '-10' and ENTER to stop");
-
-    std::string input;
-    std::cin >> input;
-    try
-    {
-      y = boost::lexical_cast<int> (input);
-    }
-    catch (boost::bad_lexical_cast const &)
-    {
-      ROS_ERROR("Error: input string was not valid.");
-      continue;
-    }
-    if (y==-10)
-      return 0;
-    if (y==1)
-      break;
-  }
 
   while (ros:: ok())
   {
@@ -324,14 +287,23 @@ int main(int argc, char **argv)
   ROS_DEBUG_STREAM("Transform "<<robot_base_link<<" <= "<<robot_end_link<<"\n"<<T_base_tool_y.matrix());
   ROS_DEBUG_STREAM("Transform "<<camera_base_link<<" <= "<<camera_marker_link<<"\n"<<T_camera_marker_y.matrix());
 
-  double distance_y=T_base_tool_y.translation()(1)-T_base_tool_x.translation()(1);
+  double distance_y=T_base_tool_y.translation()(1)-T_base_tool_orig.translation()(1);
   ROS_INFO("Do you moved the robot of %f mm? if not check the TF and restart the node",distance_y*1e3);
-  double distance_y_camera=(T_camera_marker_y.translation()-T_camera_marker_x.translation()).norm();
+  double distance_y_camera=(T_camera_marker_y.translation()-T_camera_marker_orig.translation()).norm();
   ROS_INFO("The amplitude of the movement recorded by the camera is %f mm",distance_y_camera*1e3);
 
 
+  Eigen::Vector3d y_robot_in_camera=(T_camera_marker_y.translation()-T_camera_marker_orig.translation())/distance_y;
+  double angle=std::acos(x_robot_in_camera.normalized().dot(y_robot_in_camera.normalized()))*180.0/M_PI;
+  ROS_INFO_STREAM("x robot in camera = " << x_robot_in_camera.normalized().transpose());
+  ROS_INFO_STREAM("y robot in camera = " << y_robot_in_camera.normalized().transpose());
+
+  ROS_INFO("angle between X and Y = %f deg", angle);
+
+
   // Z DIRECTION
-  ROS_INFO("\n\nMove the robot in the Z direction around +/- 200mm . Afterthat");
+  ROS_INFO("The robot will move in Z direction around +/-%f mm",amplitude*1000);
+  target_pose=initial_pose;
   while(ros::ok())
   {
     move_group.setStartState(*move_group.getCurrentState());
@@ -339,7 +311,7 @@ int main(int argc, char **argv)
     while (std::abs(delta_z)<0.05)
       delta_z=-0.200+0.4*(1.0*(double)std::rand()/((double)RAND_MAX));
     ROS_INFO("delta_z=%f",delta_z);
-    target_pose.position.z=actual_pose.position.z+delta_z;
+    target_pose.position.z=initial_pose.position.z+delta_z;
     ROS_DEBUG_STREAM("Actual\n"<<actual_pose<<"\nTarget=\n"<<target_pose);
     robot_state::RobotState target_state(*move_group.getCurrentState());
     if (!target_state.setFromIK(joint_model_group,target_pose)) continue;
@@ -365,30 +337,7 @@ int main(int argc, char **argv)
     }
   }
 
-
-
-  while (ros::ok())
-  {
-    ROS_INFO("Press");
-    ROS_INFO("- '1' and ENTER to continue");
-    ROS_INFO("- '-10' and ENTER to stop");
-
-    std::string input;
-    std::cin >> input;
-    try
-    {
-      y = boost::lexical_cast<int> (input);
-    }
-    catch (boost::bad_lexical_cast const &)
-    {
-      ROS_ERROR("Error: input string was not valid.");
-      continue;
-    }
-    if (y==-10)
-      return 0;
-    if (y==1)
-      break;
-  }
+  ros::Duration(2.0).sleep();
 
   while (ros:: ok())
   {
@@ -405,6 +354,7 @@ int main(int argc, char **argv)
       ros::Duration(1.0).sleep();
     }
   }
+
   Eigen::Affine3d T_camera_marker_z;
   Eigen::Affine3d T_base_tool_z;
   tf::poseTFToEigen(transform_base_tool,T_base_tool_z);
@@ -412,14 +362,12 @@ int main(int argc, char **argv)
   ROS_DEBUG_STREAM("Transform "<<robot_base_link<<" <= "<<robot_end_link<<"\n"<<T_base_tool_z.matrix());
   ROS_DEBUG_STREAM("Transform "<<camera_base_link<<" <= "<<camera_marker_link<<"\n"<<T_camera_marker_z.matrix());
 
-  double distance_z=T_base_tool_z.translation()(2)-T_base_tool_y.translation()(2);
+  double distance_z=T_base_tool_z.translation()(2)-T_base_tool_orig.translation()(2);
   ROS_INFO("Do you moved the robot of %f mm? if not check the TF and restart the node",distance_z*1e3);
-  double distance_z_camera=(T_camera_marker_z.translation()-T_camera_marker_y.translation()).norm();
+  double distance_z_camera=(T_camera_marker_z.translation()-T_camera_marker_orig.translation()).norm();
   ROS_INFO("The amplitude of the movement recorded by the camera is %f mm",distance_z_camera*1e3);
 
-  Eigen::Vector3d x_robot_in_camera=(T_camera_marker_x.translation()-T_camera_marker_orig.translation())/distance_x;
-  Eigen::Vector3d y_robot_in_camera=(T_camera_marker_y.translation()-T_camera_marker_x.translation())/distance_y;
-  Eigen::Vector3d z_robot_in_camera_validation=(T_camera_marker_z.translation()-T_camera_marker_y.translation())/distance_z;
+  Eigen::Vector3d z_robot_in_camera_validation=(T_camera_marker_z.translation()-T_camera_marker_orig.translation())/distance_z;
   x_robot_in_camera.normalize();
   y_robot_in_camera=y_robot_in_camera-y_robot_in_camera.dot(x_robot_in_camera)*x_robot_in_camera;
   y_robot_in_camera.normalize();
@@ -433,32 +381,72 @@ int main(int argc, char **argv)
   ROS_INFO_STREAM("z_robot_in_camera = "<<z_robot_in_camera.transpose());
   ROS_INFO_STREAM("z_robot_in_camera VALIDATION = "<<z_robot_in_camera_validation.transpose());
 
-  Eigen::Matrix3d R_camera_robot;
-  R_camera_robot<< x_robot_in_camera, y_robot_in_camera, z_robot_in_camera;
-  Eigen::Matrix3d R_robot_camera;
-  R_robot_camera=R_camera_robot.transpose();
+  Eigen::Matrix3d R_camera_base;
+  R_camera_base<< x_robot_in_camera, y_robot_in_camera, z_robot_in_camera;
+  Eigen::Matrix3d R_base_camera;
+  R_base_camera=R_camera_base.transpose();
 
 
+  // Coming home
 
+  move_group.setStartState(*move_group.getCurrentState());
+  move_group.setJointValueTarget(initial_state);
+
+  success = (move_group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  if (!success)
+  {
+    ROS_WARN("unable to plan the coming-home movement");
+    return 0;
+  }
+  else
+  {
+    ROS_INFO("planned!");
+  }
+  if (!(move_group.execute(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS))
+    ROS_WARN("unable to execute the coming-home movement");
+  else
+  {
+    actual_pose=target_pose;
+    ROS_INFO("moved");
+  }
 
   // Rotation
 
-  while(ros::ok())
+
+  Eigen::Matrix3d identity;
+  Eigen::Matrix<double,-1,-1> regressor;
+  Eigen::VectorXd data;
+
+  identity.setIdentity();
+  int irot=0;
+  regressor.resize(rotation_samples_number*3,6);
+  data.resize(rotation_samples_number*3);
+  regressor.setZero();
+  data.setZero();
+
+  while (irot<rotation_samples_number)
   {
+    target_pose=initial_pose;
     move_group.setStartState(*move_group.getCurrentState());
-    Eigen::Affine3d T_actual_pose;
-    tf::poseMsgToEigen(actual_pose,T_actual_pose);
+    Eigen::Affine3d T_initial_pose;
+    tf::poseMsgToEigen(initial_pose,T_initial_pose);
 
     double delta_rot=0;
-    while (std::abs(delta_rot)<0.025)
-      delta_rot=-0.1+0.2*(1.0*(double)std::rand()/((double)RAND_MAX));
 
-    ROS_INFO("delta_rot=%f",delta_rot);
-    Eigen::AngleAxisd q(delta_rot,Eigen::Vector3d::UnitZ());
-    T_actual_pose=T_actual_pose*q;
-    tf::poseEigenToMsg(T_actual_pose,target_pose);
+    while (std::abs(delta_rot)<0.5*amplitude_rot)
+    {
+      double tmp=(double)std::rand()/((double)RAND_MAX);
+      delta_rot=-(1.0+2.0*tmp)*amplitude_rot;
+      ROS_INFO("delta_rot=%f, amplitude_rot=%f, tmp=%f",delta_rot,amplitude_rot,tmp);
+    }
+
+    Eigen::Vector3d axis=Eigen::MatrixXd::Random(3,1);
+    axis.normalize();
+    Eigen::AngleAxisd q(delta_rot,axis);
+    T_initial_pose=T_initial_pose*q;
+    tf::poseEigenToMsg(T_initial_pose,target_pose);
     ROS_DEBUG_STREAM("Actual\n"<<actual_pose<<"\nTarget=\n"<<target_pose);
-    robot_state::RobotState target_state(*move_group.getCurrentState());
+    robot_state::RobotState target_state(initial_state);
     if (!target_state.setFromIK(joint_model_group,target_pose)) continue;
     move_group.setJointValueTarget(target_state);
 
@@ -474,56 +462,15 @@ int main(int argc, char **argv)
     }
 
     if (!(move_group.execute(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS))
+    {
       ROS_WARN("unable to execute");
-    else
-    {
-      actual_pose=target_pose;
-      ROS_INFO("moved");
-      break;
+      continue;
     }
-  }
+    actual_pose=target_pose;
+    ROS_INFO("moved");
 
+    ros::Duration(2.0).sleep();
 
-
-
-
-  Eigen::Matrix3d identity;
-  Eigen::Matrix<double,-1,-1> regressor;
-  Eigen::VectorXd data;
-
-  identity.setIdentity();
-  int irot=0;
-  regressor.resize(rotation_samples_number*3,6);
-  data.resize(rotation_samples_number*3);
-
-  while (irot<rotation_samples_number)
-  {
-    if (irot>0)
-    {
-      ROS_INFO("\n\nIteration %d of %d. Rotate the tool. Afterthat",irot+1,rotation_samples_number);
-      while (ros::ok())
-      {
-        ROS_INFO("Press");
-        ROS_INFO("- '1' and ENTER to continue");
-        ROS_INFO("- '-10' and ENTER to stop");
-
-        std::string input;
-        std::cin >> input;
-        try
-        {
-          y = boost::lexical_cast<int> (input);
-        }
-        catch (boost::bad_lexical_cast const &)
-        {
-          ROS_ERROR("Error: input string was not valid.");
-          continue;
-        }
-        if (y==-10)
-          return 0;
-        if (y==1)
-          break;
-      }
-    }
     while (ros:: ok())
     {
       try
@@ -546,35 +493,70 @@ int main(int argc, char **argv)
     ROS_DEBUG_STREAM("Transform "<<robot_base_link<<" <= "<<robot_end_link<<"\n"<<T_base_tool_rot.matrix());
     ROS_DEBUG_STREAM("Transform "<<camera_base_link<<" <= "<<camera_marker_link<<"\n"<<T_camera_marker_rot.matrix());
 
-    Eigen::Matrix<double,3,6> row;
-    row << T_base_tool_rot.matrix().block(0,0,3,3), identity;
-    regressor.block(irot*3,0,3,6)=row;
-    data.block(irot*3,0,3,1)=R_robot_camera*T_camera_marker_rot.translation()-T_base_tool_rot.translation();
+    regressor.block(irot*3,0,3,3)=T_base_tool_rot.matrix().block(0,0,3,3);
+    regressor.block(irot*3,3,3,3)=-identity;
+
+    data.block(irot*3,0,3,1)=R_base_camera*T_camera_marker_rot.translation()-T_base_tool_rot.translation();
     irot++;
   }
 
+
   Eigen::VectorXd solution=regressor.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(data);
-  Eigen::VectorXd orig_robot_camera=solution.tail(3);
+  ROS_INFO_STREAM("error = \n"<<regressor*solution-data);
+  Eigen::VectorXd orig_base_camera=solution.tail(3);
   Eigen::VectorXd orig_tool_marker=solution.head(3);
 
-  Eigen::Quaterniond q_robot_camera(R_robot_camera);
+  Eigen::Quaterniond q_base_camera(R_base_camera);
 
-  ROS_INFO_STREAM("R_robot_camera\n"<<R_robot_camera);
-  ROS_INFO_STREAM("q_robot_camera\n"<<q_robot_camera.vec()<<","<<q_robot_camera.w());
-  ROS_INFO_STREAM("orig_robot_camera\n"<<orig_robot_camera);
+  ROS_INFO_STREAM("R_base_camera\n"<<R_base_camera);
+  ROS_INFO_STREAM("q_base_camera\n"<<q_base_camera.vec()<<","<<q_base_camera.w());
+  ROS_INFO_STREAM("orig_base_camera\n"<<orig_base_camera);
   ROS_INFO_STREAM("orig_tool_marker\n"<<orig_tool_marker);
-  Eigen::Affine3d T_robot_camera;
-  T_robot_camera=q_robot_camera;
-  T_robot_camera.translation()=orig_robot_camera;
-  tf::StampedTransform t;
-  t.frame_id_=robot_base_link;
-  t.child_frame_id_=camera_base_link;
-  tf::poseEigenToTF(T_robot_camera,t);
-  ROS_INFO_STREAM("T_robot_camera\n"<<T_robot_camera.matrix());
+  Eigen::Affine3d T_base_camera;
+  T_base_camera=q_base_camera;
+  T_base_camera.translation()=orig_base_camera;
+  tf::StampedTransform tf_base_camera;
+  tf_base_camera.frame_id_=robot_base_link;
+  tf_base_camera.child_frame_id_=camera_base_link+"_test";
+  tf::poseEigenToTF(T_base_camera,tf_base_camera);
+
+  Eigen::Affine3d T_tool_marker;
+  T_tool_marker.linear()=T_base_tool_orig.linear().inverse()*T_base_camera.linear()*T_camera_marker_orig.linear();
+  T_tool_marker.translation()=orig_tool_marker;
+  tf::StampedTransform tf_tool_marker;
+  tf_tool_marker.frame_id_=robot_end_link;
+  tf_tool_marker.child_frame_id_=camera_marker_link+"_test";
+  tf::poseEigenToTF(T_tool_marker,tf_tool_marker);
+
+
+  move_group.setStartState(*move_group.getCurrentState());
+  move_group.setJointValueTarget(initial_state);
+
+  success = (move_group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  if (!success)
+  {
+    ROS_WARN("unable to plan the coming-home movement");
+    return 0;
+  }
+  else
+  {
+    ROS_INFO("planned!");
+  }
+  if (!(move_group.execute(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS))
+    ROS_WARN("unable to execute the coming-home movement");
+  else
+  {
+    actual_pose=target_pose;
+    ROS_INFO("moved");
+  }
+
+  ROS_INFO_STREAM("T_base_camera\n"<<T_base_camera.matrix());
   while (ros::ok())
   {
-    t.stamp_=ros::Time::now();
-    broadcaster.sendTransform(t);
+    tf_base_camera.stamp_=ros::Time::now();
+    broadcaster.sendTransform(tf_base_camera);
+    tf_tool_marker.stamp_=ros::Time::now();
+    broadcaster.sendTransform(tf_tool_marker);
     ros::Duration(0.05).sleep();
   }
   return 0;
